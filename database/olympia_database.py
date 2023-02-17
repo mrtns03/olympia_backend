69747632f9225428063e2065af09b1dd1f23a52f
Emil-Julius Martens <emil-julius.martens@mdcc.de>
2023-02-17 08:40:10 +0100

    kkkk

--- database/olympia_database.py
+++ database/olympia_database.py
@@ -9,20 +9,6 @@ from models.login_models import User
 engine = create_engine("sqlite:///new_olympia.db")
 
 
-def select_ergebnisse():
-    with Session(engine) as session:
-        return session.execute(select(Ergebnisse, Athleten).join(Athleten)).all()
-
-
-def select_athletes():
-    with Session(engine) as session:
-        return session.execute(
-            select(Athleten, Laender, Sportarten)
-            .join(Laender, Laender.l_pk == Athleten.a_l_pk)
-            .join(Sportarten, Sportarten.s_pk == Athleten.a_s_pk)
-        ).all()
-
-
 def get_user(username: str):
     with Session(engine) as session:
         result = session.execute(f"SELECT id, username, hashed_password FROM users WHERE username = '{username}'").one()
@@ -36,6 +22,20 @@ def insert_user(username: str, hashed_password: str):
     with Session(engine) as session:
         session.execute("INSERT INTO users (username, hashed_password) VALUES (?, ?)", (username, hashed_password))
         session.commit()
+
+
+def select_ergebnisse():
+    with Session(engine) as session:
+        return session.execute(select(Ergebnisse, Athleten).join(Athleten)).all()
+
+
+def select_athletes():
+    with Session(engine) as session:
+        return session.execute(
+            select(Athleten, Laender, Sportarten)
+            .join(Laender, Laender.l_pk == Athleten.a_l_pk)
+            .join(Sportarten, Sportarten.s_pk == Athleten.a_s_pk)
+        ).all()
 
 
 def select_athletes_by_nation(nation: str):
